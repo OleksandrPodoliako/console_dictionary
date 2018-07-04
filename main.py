d6e394d199ca58ae54c556b594ca45db24e88437
Oleksandr Podoliako <oleksandr.podoliako@eleks.com>
2018-07-04 12:20:52 +0300

    base structure

---
+++ main.py
@@ -0,0 +1,24 @@
+'''
+This is dictionary
+'''
+import json
+
+data_dictionary = json.load(open('dictionary.json','r'))
+
+while True:
+    command = raw_input('Enter R to read from dictionary, W to write new word in dictionary and E to exit: ')
+    if command.upper() == 'E':
+        print 'Good luck'
+        break
+    elif command.upper() == 'R':
+        word = raw_input('Enter word: ')
+        if len(data_dictionary[word]) > 0:
+            for description in data_dictionary[word]:
+                print '*** '+ description
+        else:
+            print 'We do not have this word in dictionary!'
+        pass
+    elif command.upper() == 'W':
+        pass
+    else:
+        print 'Invalid command'
